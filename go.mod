7991750026ebe79188aa2150e36668d8d253deee
sampork <sampork@live.com>
2023-12-29 22:24:52 +0800

    update redis

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/40t/go-sniffer
 
 go 1.21
 
-require github.com/google/gopacket v1.1.19 // indirect
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/secmask/go-redisproto v0.1.0 // indirect
+)
--- plugSrc/redis/build/entry.go
+++ plugSrc/redis/build/entry.go
@@ -6,14 +6,17 @@ import (
 	"strings"
 	"fmt"
 	"strconv"
-	"bufio"
+	"math/big"
+	"time"
+
+	"github.com/40t/go-sniffer/plugSrc/redis/build/go-redis-internal/proto"
+	"github.com/google/gopacket/tcpassembly/tcpreader"
+	"github.com/secmask/go-redisproto"
 )
 
 type Redis struct {
 	port int
 	version string
-	cmd chan string
-	done chan bool
 }
 
 const (
@@ -32,46 +35,100 @@ func NewInstance() *Redis{
 }
 
 func (red Redis) ResolveStream(net, transport gopacket.Flow, r io.Reader) {
-
-	buf := bufio.NewReader(r)
-	var cmd string
-	var cmdCount = 0
-	for {
-
-		line, _, _ := buf.ReadLine()
-
-		if len(line) == 0 {
-			buff := make([]byte, 1)
-			_, err := r.Read(buff)
-			if err == io.EOF {
-				red.done <- true
-				return
-			}
-		}
-
-		//Filtering useless data
-		if !strings.HasPrefix(string(line), "*") {
-			continue
-		}
-
-		//Do not display
-		if strings.EqualFold(transport.Src().String(), strconv.Itoa(red.port)) == true {
-			continue
-		}
-
-		//run
-		l := string(line[1])
-		cmdCount, _ = strconv.Atoi(l)
-		cmd = ""
-		for j := 0; j < cmdCount * 2; j++ {
-			c, _, _ := buf.ReadLine()
-			if j & 1 == 0 {
-				continue
-			}
-			cmd += " " + string(c)
-		}
-		fmt.Println(cmd)
+	if strings.EqualFold(transport.Src().String(), strconv.Itoa(red.port)) {
+		resolveServerPacket(net, transport, r)
+	} else {
+		resolveClientPacket(net, transport, r)
 	}
+}
+
+func resolveClientPacket(net, transport gopacket.Flow, r io.Reader) {
+	defer tcpreader.DiscardBytesToEOF(r)
+	parser := redisproto.NewParser(r)
+	for {
+		req, err := parser.ReadCommand()
+		fmt.Printf("\n%s %s:%s->%s:%s\n", time.Now().Format("2006-01-02 15:04:05.000000"), net.Src().String(), transport.Src().String(), net.Dst().String(), transport.Dst().String())
+		if err == io.EOF {
+			fmt.Println("# End of stream")
+			return
+		} else if err != nil {
+			fmt.Println("# Error reading stream", ":", err)
+			return
+		} else {
+			var s string
+			for i := 0; i <= req.ArgCount(); i++ {
+				s += string(req.Get(i)) + " "
+			}
+			fmt.Println(s)
+		}
+	}
+}
+
+func resolveServerPacket(net, transport gopacket.Flow, r io.Reader) {
+	defer tcpreader.DiscardBytesToEOF(r)
+	reader := proto.NewReader(r)
+	for {
+		resp, err := reader.ReadReply()
+		fmt.Printf("\n%s %s:%s->%s:%s\n", time.Now().Format("2006-01-02 15:04:05.000000"), net.Src().String(), transport.Src().String(), net.Dst().String(), transport.Dst().String())
+		if err == io.EOF {
+			fmt.Println("# End of stream")
+			return
+		} else if _, ok := err.(proto.RedisError); ok {
+			fmt.Print(dumpResponse(err, ""))
+		} else if err != nil {
+			fmt.Println("# Error reading stream", ":", err)
+			return
+		} else {
+			fmt.Print(dumpResponse(resp, ""))
+		}
+	}
+}
+
+func dumpResponse(resp interface{}, spaces string) string {
+	str := ""
+	switch resp := resp.(type) {
+	case nil:
+		str += fmt.Sprintf("%s\n", "(Null)")
+	case int64:
+		str += fmt.Sprintf("%s%+v\n", "(Number)", resp)
+	case float64:
+		str += fmt.Sprintf("%s%+v\n", "(Double)", resp)
+	case bool:
+		str += fmt.Sprintf("%s%+v\n", "(Boolean)", resp)
+	case *big.Int:
+		str += fmt.Sprintf("%s%+v\n", "(BigNumber)", resp)
+	case string:
+		str += fmt.Sprintf("%s%+v\n", "(String)", resp)
+	case []interface{}:
+		arr := resp
+		str += fmt.Sprintf("%s%+v\n", "(Array)", len(arr))
+		for i, v := range arr {
+			s := strconv.Itoa(i) + " "
+			str += fmt.Sprintf("%s%s", spaces, s)
+			str += dumpResponse(v, spaces+strings.Repeat(" ", len(s)))
+		}
+		str += "\n"
+	case map[interface{}]interface{}:
+		m := resp
+		str += fmt.Sprintf("%s%+v\n", "(Map)", len(m))
+		for k, v := range m {
+			s := fmt.Sprintf("%v", k) + ":"
+			str += fmt.Sprintf("%s%s", spaces, s)
+			str += dumpResponse(v, spaces+strings.Repeat(" ", len(s)))
+		}
+		str += "\n"
+	case proto.RedisError:
+		if resp == proto.Nil {
+			str += fmt.Sprintf("%s\n", "(Null)")
+		} else {
+			str += fmt.Sprintf("%s%+v\n", "(Error)", resp)
+		}
+
+	default:
+		str += fmt.Sprintf("%s%+v\n", "(Unknow)", resp)
+	}
+
+	return str
 }
 
 /**
